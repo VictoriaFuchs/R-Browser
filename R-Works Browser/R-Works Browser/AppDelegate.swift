937bfec3fdd8ecd86153bd2f698fa76c9f1fc83d
VictoriaFuchs <VicFuchs@bondandroark.com>
2017-03-18 14:17:30 -0400

    Add files via upload

---
+++ R-Works Browser/R-Works Browser/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  R-Works Browser
+//
+//  Created by System Administrator on 3/15/17.
+//  Copyright © 2017 R-Works. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ R-Works Browser/R-Works Browser/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  R-Works Browser
+//
+//  Created by System Administrator on 3/15/17.
+//  Copyright © 2017 R-Works. All rights reserved.
+//
+
+import Cocoa
+
+
+class ViewController: NSViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
